a3606fa8cb8a139f2d4fe5ae4b635e1865936ce9
Harrison.Yao <harrison.gyao@gmail.com>
2012-08-28 17:10:49 +0800

    complete the encode method

--- mywaiter/src/main/java/cn/com/mywaiter/util/QRCodeGenarator.java
+++ mywaiter/src/main/java/cn/com/mywaiter/util/QRCodeGenarator.java
@@ -3,10 +3,47 @@
  */
 package cn.com.mywaiter.util;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
 /**
  * @author Harrison
- *
+ * 
  */
 public class QRCodeGenarator {
+
+	/**
+	 * 
+	 * @param contents
+	 * @param width
+	 * @param height
+	 * @param imgPath
+	 */
+	public void encode(String contents, int width, int height, String imgPath) {
+		Map<EncodeHintType, Object> hints = new HashMap<EncodeHintType, Object>();
+		// 指定纠错等级
+		hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+		// 指定编码格式
+		hints.put(EncodeHintType.CHARACTER_SET, "GBK");
+		try {
+			BitMatrix bitMatrix = new MultiFormatWriter().encode(contents,
+					BarcodeFormat.QR_CODE, width, height, hints);
+
+			MatrixToImageWriter
+					.writeToFile(bitMatrix, "png", new File(imgPath));
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
 
 }
